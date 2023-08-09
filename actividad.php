d2234ec01203b9001fb74994d0521d9aef16bffa
lucas-chuan <luckiespinosa2004@gmail.com>
2023-08-09 20:11:50 -0300

    a

---
+++ actividad.php
@@ -0,0 +1,36 @@
+<?php 
+
+function MOSTRARDATOS($resultados){
+    if ($resultados != NULL) {
+    echo "nombre ".$resultados['nombre']."<br/>";
+    echo "apellido ".$resultados['apellido']."<br/>";
+    echo "telefono ".$resultados['telefono']."<br/>";
+    echo "edad ".$resultados['edad']."<br/>";
+    echo "altura ".$resultados['altura']."<br/>";
+    echo "peso ".$resultados['peso']."<br/>";
+    } else {
+    echo "<br/>no hay mas datos<br/>";
+    }
+    }
+    
+    $link = mysqli_connect("localhost", "lucas", "1234","escuela");
+    if($link){
+        echo "ok";
+    }else{
+        echo 'error';
+    }
+    /*mysqli_select_db($link, "TAREAS");
+    mysqli_query($link, "SET NAMES UTF8");
+    
+    $resultados = mysqli_query($link, "SELECT * FROM alumnos");
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    MOSTRARDATOS(mysqli_fetch_array($resultados));
+    
+    mysqli_free_result($resultados);
+    mysqli_close($link);*/
+
+?>
