9cd934206deac2994c65f37e9ebfb82334e5dbb8
lucas-chuan <luckiespinosa2004@gmail.com>
2023-08-30 16:19:26 -0300

    a

--- actividad.php
+++ actividad.php
@@ -1,23 +1,5 @@
 <?php 
 
     include("conexion.php");
-    
-    $nombre = $_REQUEST['nombre'];
-    $apellido = $_REQUEST['apellido'];
-    $telefono = $_REQUEST['telefono'];
-    $edad = $_REQUEST['edad'];
-    $altura = $_REQUEST['altura'];
-    $peso = $_REQUEST['peso'];
 
-    echo "hola ".$nombre;
-    echo "<br>";
-    echo $apellido;
-    echo "<br>";
-    echo $telefono;
-    echo "<br>";
-    echo $edad;    
-    echo "<br>";
-    echo $altura;
-    echo "<br>";
-    echo $peso;
     ?>    
